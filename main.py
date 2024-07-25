dd8379f85c85dc90c3cd34b04641a84956264531
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-07-25 10:26:27 +0000

    YTMP4 BOT

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("download.default_directory=C:/Ai-Scraping")
 
 driver = webdriver.Chrome(options=options)
 
@@ -25,14 +26,33 @@ element = driver.find_element(By.XPATH, '//input[@id="input"]')
 element.send_keys('https://youtu.be/dQw4w9WgXcQ?si=u2VkmDzC-oXxU_yy')
 
 
-submit = driver.find_element(By.XPATH, '//input[@id="submit"]').click()
+driver.find_element(By.XPATH, '//input[@id="submit"]').click()
 
-driver.execute_script("window.scrollTo(0, 400);")
 
-time.sleep(5)
 
 url = driver.find_element(By.XPATH, "//iframe[@id='buttonApi']").get_attribute('src')
 
-driver.get(url)
+driver.get(str(url))
 
+time.sleep(2)
+videoButton = driver.find_element(By.XPATH, '//button[contains(text(), "Video")]')
+
+videoButton.click()
+
+time.sleep(2)
+
+downloadButton = driver.find_element(By.XPATH, '//tr[2]//a[contains(text(), " Download")]')
+
+downloadButton.click()
+
+time.sleep(15)
+
+downloadButton2 = driver.find_element(By.XPATH, '//span[contains(text(), "Download")]')
+
+
+downloadButton2.click()
 time.sleep(5)
+print(driver.current_url)
+time.sleep(15)
+
+
